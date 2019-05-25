e0daccc36058b62a1e7edb6d1c35867ccf2c4da6
Arto Chydenius <arto.chydenius@futurice.com>
2019-05-25 22:52:31 +0300

    Output block data as C source code

--- png2src.py
+++ png2src.py
@@ -4,32 +4,41 @@ from itertools import zip_longest
 PNG_FILE = "test.png"
 
 
-def pack_pixels(pixels):
-    """Pack an array of continuous rgb values to tuples:
-    e.g. [0, 1, 2, 3, 3, 3] -> [(0, 1, 2), (3, 3, 3)]"""
-    return list(zip(*[iter(pixels)] * 3))
+def get_pixel(r, g, b):
+    """Get corresponding ZX Spectrum color index"""
+    # TODO: Real functionality
+    return 0 if r == 0 and g == 0 and b == 0 else 7
 
 
-def is_block_empty(rows):
-    """Define if all pixels in block are empty"""
-    return all(all(pixel == (0, 0, 0) for pixel in cols) for cols in rows)
+def is_empty(blocks):
+    """Define if all pixels in row of blocks are empty"""
+    return all(all(pixel == 0 for pixel in block) for block in blocks)
 
 
 def read_block(data, y, x):
     """Read an 8x8 pixel block from input data"""
-    return [pack_pixels(row[x:x + 24]) for row in data[y:y + 8]]
+    block = []
+    for row in data[y:y + 8]:
+        for i in range(x, x + 24, 3):
+            block.append(get_pixel(row[i], row[i + 1], row[i + 2]))
+
+    return block
 
 width, height, data, _ = png.Reader(PNG_FILE).read()
 pixels = list(data)
 
+# Read all blocks
 blocks = []
 for i in range(0, height, 8):
-    row = [read_block(pixels, i, j) for j in range(0, width, 8)]
+    row = [read_block(pixels, i, j) for j in range(0, width * 3, 24)]
 
-    # Exit on completely empty row of blocks
-    if all(is_block_empty(block) for block in row):
+    # Exit on first completely empty row of blocks
+    if is_empty(row):
         break
     else:
-        blocks.append(row)
+        blocks += row
 
-print(blocks)
+# Print source code
+print("char data[{0}][{1}] = {{".format(len(blocks), len(blocks[0])))
+print(",\n".join("    {{ {0} }}".format(",".join(str(pixel) for pixel in block)) for block in blocks))
+print("};")
